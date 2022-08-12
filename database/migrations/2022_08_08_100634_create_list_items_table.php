27bc738d1a49bd459ff232048634bb0d9555e51e
Daniel Nwaeke <dannie005@gmail.com>
2022-08-12 13:39:44 +0300

    added the table drop on the new migration file

--- database/migrations/2022_08_08_100634_create_list_items_table.php
+++ database/migrations/2022_08_08_100634_create_list_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('list_items');
         Schema::create('list_items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
